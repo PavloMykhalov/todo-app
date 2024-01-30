68c17013e388cfcab78d70240622a677800ec56b
Pavlo Mykhalov <pavlomihaliov@gmail.com>
2024-01-30 11:52:37 +0200

    add config for vercel db

--- src/services/db.js
+++ src/services/db.js
@@ -1,6 +1,23 @@
 import { Sequelize } from 'sequelize';
 
-export const sequelize = new Sequelize('postgres', 'postgres', '123456789', {
-  host: 'localhost',
-  dialect: 'postgres',
-});
+const {
+  POSTGRES_DATABASE,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  POSTGRES_HOST,
+} = process.env;
+
+export const sequelize = new Sequelize(
+  POSTGRES_DATABASE || 'verceldb',
+  POSTGRES_USER || 'default',
+  POSTGRES_PASSWORD || 'CYFZn7t9AWmI',
+  {
+    host: POSTGRES_HOST || 'ep-noisy-river-a2edgone-pooler.eu-central-1.postgres.vercel-storage.com',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+  }
+);
